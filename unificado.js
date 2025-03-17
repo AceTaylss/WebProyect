a545c83b02d88079f64bb225324e10d1a27b2e1f
AceTayl <126363648+AceTaylss@users.noreply.github.com>
2025-03-17 11:37:59 -0300

    Add files via upload

---
+++ unificado.js
@@ -0,0 +1,105 @@
+// INICIO DEL ARCHIVO JS UNIFICADO
+// Código de app.js
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}            
+
+// Código de produc.js
+var swiper = new Swiper('.product-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: false,
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },
+
+    on: {
+        init: function(){
+            var index = this.activeIndex;
+
+            var target = $('.product-slider__item').eq(index).data('target');
+
+            console.log(target);
+
+            $('.product-img__item').removeClass('active');
+            $('.product-img__item#'+ target).addClass('active');
+        }
+    }
+
+});
+
+swiper.on('slideChange', function () {
+    var index = this.activeIndex;
+
+    var target = $('.product-slider__item').eq(index).data('target');
+
+    console.log(target);
+
+    $('.product-img__item').removeClass('active');
+    $('.product-img__item#'+ target).addClass('active');
+
+    if(swiper.isEnd) {
+        $('.prev').removeClass('disabled');
+        $('.next').addClass('disabled');
+    } else {
+        $('.next').removeClass('disabled');
+    }
+
+    if(swiper.isBeginning) {
+        $('.prev').addClass('disabled');
+    } else {
+        $('.prev').removeClass('disabled');
+    }
+});
+
+$(".js-fav").on("click", function() {
+    $(this).find('.heart').toggleClass("is-active");
+});
+            
+
